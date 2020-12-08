2e5063dd80603a36ab93b8438170bb86c5f692e3
Pedro Sariego <pedro@sariego.dev>
2020-12-08 20:05:13 -0300

    add domain layer tests

---
+++ app/src/test/java/dev/sariego/reignhiringtest/domain/InteractorsTest.kt
@@ -0,0 +1,53 @@
+package dev.sariego.reignhiringtest.domain
+
+import dev.sariego.reignhiringtest.ArticleFactory
+import dev.sariego.reignhiringtest.domain.interactor.DeleteArticle
+import dev.sariego.reignhiringtest.domain.interactor.ObserveArticles
+import dev.sariego.reignhiringtest.domain.interactor.UpdateArticles
+import dev.sariego.reignhiringtest.domain.repository.ArticlesRepository
+import io.mockk.MockKAnnotations
+import io.mockk.coVerify
+import io.mockk.impl.annotations.MockK
+import io.mockk.verify
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class InteractorsTest {
+
+    @MockK
+    lateinit var repository: ArticlesRepository
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this, relaxed = true)
+    }
+
+    @Test
+    fun observeInvocation_usesRepositoryStream() {
+        val observeInteractor = ObserveArticles(repository)
+
+        observeInteractor()
+        verify { repository.stream() }
+    }
+
+    @Test
+    fun updateInvocation_usesRepositoryFetch() = runBlockingTest {
+        val updateInteractor = UpdateArticles(repository)
+
+        updateInteractor()
+        coVerify { repository.fetch() }
+    }
+
+    @Test
+    fun deleteInvocation_usesRepositoryDelete() = runBlockingTest {
+        val expected = ArticleFactory.make()
+        val deleteInteractor = DeleteArticle(repository)
+
+        deleteInteractor(expected)
+        coVerify { repository.delete(expected) }
+    }
+
+}
