c47c65515ca8491b9e15cf78b70bb6b8d6f80fcd
Pedro Sariego <pedro@sariego.dev>
2020-12-13 14:56:47 -0300

    add remote mapper tests

--- app/src/sharedTest/java/dev/sariego/reignhiringtest/test/factory/DataArticleFactory.kt
+++ app/src/sharedTest/java/dev/sariego/reignhiringtest/test/factory/DataArticleFactory.kt
@@ -1,6 +1,7 @@
 package dev.sariego.reignhiringtest.test.factory
 
 import dev.sariego.reignhiringtest.framework.db.LocalDataArticle
+import dev.sariego.reignhiringtest.framework.net.RemoteDataArticle
 import io.github.serpro69.kfaker.Faker
 import java.net.URL
 import java.util.*
@@ -24,4 +25,18 @@ object DataArticleFactory {
 
     fun makeLocalList(capacity: Int = 10): List<LocalDataArticle> =
         List(capacity) { makeLocal() }
+
+    fun makeRemote(): RemoteDataArticle {
+        val uuid = UUID.randomUUID()
+        val url = URL("https", faker.internet.domain(), "/$uuid")
+        return RemoteDataArticle(
+            id = uuid.hashCode(),
+            title = faker.movie.title(),
+            storyTitle = faker.movie.title(),
+            author = faker.artist.names(),
+            created = Date(),
+            url = url.toString(),
+            storyUrl = url.toString(),
+        )
+    }
 }
